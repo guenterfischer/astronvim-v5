b43243c1948c0e8d85b64c7265e1f6543b308862
Günter Fischer <fischer-guenter@gmx.net>
2026-03-07 20:10:04 +0000

    Added codecompanion

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,13 +1,61 @@
--- You can also add or configure plugins by creating files in this `plugins/` folder
--- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
--- Here are some examples:
-
 ---@type LazySpec
 return {
-  "svrana/neosolarized.nvim",
-  dependencies = { "tjdevries/colorbuddy.nvim" },
-  opts = {
-    comment_italics = true,
-    background_set = true,
+  {
+    "svrana/neosolarized.nvim",
+    dependencies = { "tjdevries/colorbuddy.nvim" },
+    opts = {
+      comment_italics = true,
+      background_set = true,
+    },
+  },
+  {
+    -- See https://codecompanion.olimorris.dev/ for details
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      adapters = {
+        gemini = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            env = {
+              api_key = os.getenv "GEMINI_API_KEY",
+            },
+            schema = {
+              model = {
+                default = (os.getenv "GEMINI_MODEL" or "gemini-2.5-flash"),
+              },
+            },
+          })
+        end,
+      },
+      interactions = {
+        chat = {
+          adapter = "gemini",
+          model = os.getenv "GEMINI_MODEL" or "gemini-2.5-flash",
+        },
+        inline = {
+          adapter = "gemini",
+          model = os.getenv "GEMINI_MODEL" or "gemini-2.5-flash",
+        },
+        cmd = {
+          adapter = "gemini",
+          model = os.getenv "GEMINI_MODEL" or "gemini-2.5-flash",
+        },
+        -- background = {
+        --   adapter = "gemini",
+        --   model = os.getenv "GEMINI_MODEL" or "gemini-2.5-flash",
+        -- },
+      },
+    },
+    keys = {
+      -- Open a chat window
+      { "<leader>ac", "<cmd>CodeCompanionChat<cr>", desc = "CodeCompanion Chat" },
+      -- Show actions
+      { "<leader>aa", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion Actions" },
+      -- Apply inline fix/generation (visual mode)
+      { "<leader>ac", ":<C-u>CodeCompanion", mode = { "v" }, desc = "CodeCompanion" },
+    },
   },
 }
